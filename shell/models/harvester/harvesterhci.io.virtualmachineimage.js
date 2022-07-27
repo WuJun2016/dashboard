a3e3f3d80a563002169ef1a3347f0184a12e7dfe
wujun <897415845@qq.com>
2022-07-27 10:50:12 +0800

    HARVESTER: delete displayName RFC1123 validations

--- shell/models/harvester/harvesterhci.io.virtualmachineimage.js
+++ shell/models/harvester/harvesterhci.io.virtualmachineimage.js
@@ -10,7 +10,7 @@ import { formatSi } from '@shell/utils/units';
 import { ucFirst } from '@shell/utils/string';
 import { stateDisplay, colorForState } from '@shell/plugins/dashboard-store/resource-class';
 import SteveModel from '@shell/plugins/steve/steve-class';
-import { _CLONE, _EDIT } from '@shell/config/query-params';
+import { _CLONE } from '@shell/config/query-params';
 
 export function isReady() {
   function getStatusConditionOfType(type, defaultValue = []) {
@@ -53,6 +53,11 @@ export default class HciVmImage extends SteveModel {
       },
       ...out
     ];
+  }
+
+  detailPageHeaderActionOverride() {
+    // https://github.com/harvester/harvester/issues/2563#issuecomment-1196320939
+    return this.displayName;
   }
 
   applyDefaults(resources = this, realMode) {
@@ -240,24 +245,16 @@ export default class HciVmImage extends SteveModel {
       out.push(fileRequired);
     }
 
-    const validateName = {
-      nullable:       false,
-      path:           'spec.displayName',
-      required:       true,
-      minLength:      1,
-      maxLength:      63,
-      translationKey: 'generic.name',
-    };
-
-    const route = this.currentRoute();
-
-    const mode = route.query?.mode;
-
-    if (mode !== _EDIT) { // compatible with the already created image
-      validateName.validators = ['displayName'];
-    }
-    out.push(validateName);
-
-    return out;
+    return [
+      {
+        nullable:       false,
+        path:           'spec.displayName',
+        required:       true,
+        minLength:      1,
+        maxLength:      63,
+        translationKey: 'generic.name',
+      },
+      ...out
+    ];
   }
 }
