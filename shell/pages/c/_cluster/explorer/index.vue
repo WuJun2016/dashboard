fcafdee9bea59c4a60aa47d8f88577db0acef095
Richard Cox <richard.cox@suse.com>
2023-03-01 16:04:34 +0000

    Fix changing grafana dashboard details for new Monitoring version - Latest monitoring chart `102.0.0+up40.1.2` bumps grafana from `7.5.11` to `9.1.5` - This breaks changing dashboard `detail/summary`, `Range` and `Refresh Rate` - It seems we're safe now to remove the dep on angular when applying these changes to the iframe url - to be replaced with a simpler approach
    Also
    - Removed the metrics tabs dependency on hash (given there's two tab groups on page)
    - Added `key` to `GrafanaDashboard` components to insure they're created/destroyed correctly
    
    > There's some whitespace changes dont automatically
    
    > Note - This cannot be tested locally due to https://github.com/rancher/rancher/issues/31891

--- shell/pages/c/_cluster/explorer/index.vue
+++ shell/pages/c/_cluster/explorer/index.vue
@@ -607,6 +607,8 @@ export default {
     </div>
     <Tabbed
       v-if="hasMetricsTabs"
+      default-tab="cluster-metrics"
+      :use-hash="false"
       class="mt-30"
       :use-hash="false"
     >
